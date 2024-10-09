bd42bdeac505efefd18948b09ee886b12a47d763
Nikhil Tomar <80913766+PREDATOR9289@users.noreply.github.com>
2024-10-09 20:44:49 +0530

    Add files via upload

---
+++ numpy_library.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# used to work on large data set
+# range is very large
+# high order function
+# array reshape
+# create n dimensional array
+
+#assign array in numpy
+
+myData = np.array([1,2,3,4])
+# print(myData)
+# print(type(myData))
+
+# r = 12
+# i = 0
+
+# while r > 8:
+#     myData[i] = r
+#     i = i + 1
+#     r = r - 1
+
+# print(myData)
+
+myFriends = np.array(["nikhil","harsh","deep","shagy"])
+for name in myFriends:
+    print(name)
